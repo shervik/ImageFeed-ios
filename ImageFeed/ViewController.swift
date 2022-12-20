7f36e917ad89164a0fd99c248ef961d2e6a59a34
Victoria Scherbakova <gesvik5@gmail.com>
2022-12-20 19:51:09 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Виктория Щербакова on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
